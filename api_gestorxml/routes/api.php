410d2aec5ebe9bdeb5a92479c22a64e4729c43e8
Diego89git <dtoapanta1240@uta.edu.ec>
2023-11-27 10:13:52 -0500

    Configuracion Seguridad API Laravel Sanctum

--- api_gestorxml/routes/api.php
+++ api_gestorxml/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('auth/register',[AuthController::class,'create']);
+Route::post('auth/login',[AuthController::class,'login']);
+
+//rutas protegidas
+Route::middleware(['auth:sanctum'])->group(function(){
+    //aqui rutas protegidas
+    Route::get('auth/logout',[AuthController::class,'logout']);
 });
+
+
